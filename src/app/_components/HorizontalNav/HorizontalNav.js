646494f60e4026274fc2bc959593757a2cd7730f
Ash Kyd <ash@kyd.com.au>
2025-09-24 21:44:49 +1000

    feature: active link style

--- src/app/_components/HorizontalNav/HorizontalNav.js
+++ src/app/_components/HorizontalNav/HorizontalNav.js
@@ -1,10 +1,16 @@
+"use client";
 import Link from "next/link";
 import "./horizontalNav.css";
+import { usePathname } from "next/navigation";
 export default function ({ items }) {
+  const pathname = usePathname();
   return (
     <ol className="horizontal-nav">
       {items.map((item) => (
-        <li key={item.url}>
+        <li
+          key={item.url}
+          className={pathname === item.url ? "horizontal-nav__active" : ""}
+        >
           <Link href={item.url}>{item.shortTitle}</Link>
         </li>
       ))}
